fd3c43bc97a3be13cec3d432174fe2eb737038d3
kirti734 <145695522+kirti734@users.noreply.github.com>
2025-10-15 08:39:09 +0530

    Update Student_Performance_Analyzer.c

--- Student_Performance_Analyzer.c
+++ Student_Performance_Analyzer.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SUBJECT_COUNT 3
+
+enum Grade {A ,B, C, D, F};
+
 struct Student
 {
 	int rollNumber;
 	char studentName[100];
-	int marks[3];
+	int marks[SUBJECT_COUNT];
 };
 
 void sortRollNumbers(int listofStudents[], int numberofStudents)
@@ -15,6 +19,7 @@ void sortRollNumbers(int listofStudents[], int numberofStudents)
     {
     	return;
 	}
+	
 	int i;
 	int rollTemp;
         
@@ -31,21 +36,95 @@ void sortRollNumbers(int listofStudents[], int numberofStudents)
     sortRollNumbers(listofStudents, numberofStudents - 1);
 }
 
+enum Grade calculateGrade(float averageMarks) 
+{
+    if (averageMarks >= 85)
+    {
+    	 return A;
+	}
+    else if (averageMarks >= 70) 
+	{
+		return B;
+	}
+    else if (averageMarks >= 50) 
+    {
+    	return C;
+	}
+    else if (averageMarks >= 35) 
+    {
+    	return D;
+	}
+    else 
+	{
+    	return F;
+	}
+}
+
+char getGrade(enum Grade grade)
+{
+	switch(grade)
+	{
+		case A:
+			return 'A';
+			
+		case B:
+			return 'B';
+			
+		case C:
+			return 'C';
+			
+		case D:
+			return 'D';
+			
+		default:
+			return 'F';			
+	}
+}
+
+const char* performanceStars(enum Grade grade) 
+{
+    switch(grade)
+    {
+    	case A:
+			return "*****";
+			
+		case B:
+			return "****";
+			
+		case C:
+			return "***";
+			
+		case D:
+			return "**";
+			
+		default:
+			return "";	
+	}
+}
+
+int calculateTotal(int marks[])
+{
+	int i = 0;
+	int marksTotal = 0;
+	
+	for(i = 0; i < SUBJECT_COUNT; i++)
+	{
+		marksTotal += marks[i];
+	}
+	
+	return marksTotal;
+}
+
 void performanceAnalyzer(struct Student students[] , int numberofStudents)
 {
 	int i = 0;
-	int j = 0;
-	int marksTotal = 0;
+	int marksTotal;
 	float marksAverage;
 	int listofStudents[numberofStudents];
 	
 	while(i < numberofStudents)
 	{
-		marksTotal = 0;
-		for(j = 0; j < 3; j++)
-		{
-			marksTotal += students[i].marks[j];
-		}
+		marksTotal = calculateTotal(students[i].marks);
 		
 		marksAverage = marksTotal / 3.0;
 		listofStudents[i] = students[i].rollNumber;
@@ -55,33 +134,15 @@ void performanceAnalyzer(struct Student students[] , int numberofStudents)
 		printf("Total: %d\n", marksTotal);
 		printf("Average %0.2f\n", marksAverage);
 		
-		if (marksAverage >= 85)
-		{
-		    printf("Grade: A\n");
-		    printf("Performance: *****\n");
-		}
-		else if (marksAverage >= 70)
-		{
-		    printf("Grade: B\n");
-		    printf("Performance: ****\n");
-		}
-		else if (marksAverage >= 50)
-		{
-		    printf("Grade: C\n");
-		    printf("Performance: ***\n");
-		}
-		else if (marksAverage >= 35)
-		{
-		    printf("Grade: D\n");
-		    printf("Performance: **\n");
-		}
-		else
-		{
-		    printf("Grade: F\n");
-		}
+		enum Grade grade = calculateGrade(marksAverage);
+		
+		printf("Grade: %c\n", getGrade(grade));
+		printf("Performance: %s\n", performanceStars(grade));
 		
 		printf("\n");
+		
 		i++;
+		
 	}
 	
 	i = 0;
@@ -97,23 +158,53 @@ void performanceAnalyzer(struct Student students[] , int numberofStudents)
 	
 }
 
-int main()
+void insertStudentDetails(struct Student students[] , int numberofStudents)
 {
-	int numberofStudents = 0;
-	scanf("%d", & numberofStudents);
-	
-	struct Student students[numberofStudents];
 	int i;
 	int j;
+	int subjectMarks;
 	
 	for (i = 0 ;i < numberofStudents ; i++)
 	{
-		scanf("%d %s", &students[i].rollNumber, students[i].studentName);
-		for (j = 0; j < 3; j++)
+		scanf("%d %99s", &students[i].rollNumber, students[i].studentName);
+		
+		for (j = 0; j < SUBJECT_COUNT; j++)
 		{
-			scanf("%d", &students[i].marks[j]);
+			if (scanf("%d", &subjectMarks) != 1)
+			{
+				printf("Marks should be a numeric value");
+				return;
+			}
+			
+			if (subjectMarks < 0)
+			{
+				printf("Marks should be a positive value");
+				return;
+			}
+
+			students[i].marks[j] = subjectMarks;
 		}
 	}
+}
+
+int main()
+{
+	int numberofStudents = 0;
 	
+	if (scanf("%d", & numberofStudents) != 1)
+	{
+		printf("Number of students should be a numeric value");
+		return;
+	}
+
+	if (numberofStudents < 0)
+	{
+		printf("Number of students should be a positive value");
+		return;
+	}
+	
+	struct Student students[numberofStudents];
+	
+	insertStudentDetails(students , numberofStudents);
 	performanceAnalyzer(students , numberofStudents);
 }
