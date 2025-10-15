51c47672d21f58d7c74d492987484a046dbb4919
kirti734 <145695522+kirti734@users.noreply.github.com>
2025-10-15 17:59:42 +0530

    Update Student_Performance_Analyzer.c

--- Student_Performance_Analyzer.c
+++ Student_Performance_Analyzer.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #define SUBJECT_COUNT 3
+#define NAME_LENGTH 100
 
 enum Grade {A ,B, C, D, F};
 
 struct Student
 {
 	int rollNumber;
-	char studentName[100];
+	char studentName[NAME_LENGTH];
 	int marks[SUBJECT_COUNT];
 };
 
@@ -20,20 +19,32 @@ void sortRollNumbers(int listofStudents[], int numberofStudents)
     	return;
 	}
 	
-	int i;
-	int rollTemp;
+	int index;
+	int tempRollNumber;
         
-    for (i = 0; i < numberofStudents - 1; i++)
+    for (index = 0; index < numberofStudents - 1; index++)
     {
-        if (listofStudents[i] > listofStudents[i + 1])
+        if (listofStudents[index] > listofStudents[index + 1])
         {
-            rollTemp = listofStudents[i];
-            listofStudents[i] = listofStudents[i + 1];
-            listofStudents[i + 1] = rollTemp;
+            tempRollNumber = listofStudents[index];
+            listofStudents[index] = listofStudents[index + 1];
+            listofStudents[index + 1] = tempRollNumber;
         }
     }
     
     sortRollNumbers(listofStudents, numberofStudents - 1);
+}
+
+void printRollNumber(int listofStudents[], int numberofStudents, int index)
+{
+	if(index >= numberofStudents)
+	{
+		return;
+	}
+	
+	printf("%d ", listofStudents[index]);
+	
+	printRollNumber(listofStudents, numberofStudents, index+1);
 }
 
 enum Grade calculateGrade(float averageMarks) 
@@ -115,23 +126,30 @@ int calculateTotal(int marks[])
 	return marksTotal;
 }
 
+float calculateAverage(int totalMarks)
+{
+	int marksAverage;
+	marksAverage = totalMarks / (float)(SUBJECT_COUNT);
+	return marksAverage;
+}
+
 void performanceAnalyzer(struct Student students[] , int numberofStudents)
 {
 	int i = 0;
-	int marksTotal;
+	int totalMarks;
 	float marksAverage;
 	int listofStudents[numberofStudents];
 	
 	while(i < numberofStudents)
 	{
-		marksTotal = calculateTotal(students[i].marks);
+		totalMarks = calculateTotal(students[i].marks);
 		
-		marksAverage = marksTotal / 3.0;
+		marksAverage = calculateAverage(totalMarks);
 		listofStudents[i] = students[i].rollNumber;
 		
 		printf("Roll: %d\n", students[i].rollNumber);
 		printf("Name: %s\n", students[i].studentName);
-		printf("Total: %d\n", marksTotal);
+		printf("Total: %d\n", totalMarks);
 		printf("Average %0.2f\n", marksAverage);
 		
 		enum Grade grade = calculateGrade(marksAverage);
@@ -141,34 +159,22 @@ void performanceAnalyzer(struct Student students[] , int numberofStudents)
 		
 		printf("\n");
 		
-		i++;
-		
+		i++;	
 	}
 	
 	i = 0;
-	printf("List of Roll Number (via recursion): ");
 		
 	sortRollNumbers(listofStudents , numberofStudents);
-		
-	while (i < numberofStudents)
-	{
-		printf("%d ", listofStudents[i]);
-		i++;
-	}
 	
+	printf("List of Roll Number (via recursion): ");
+	printRollNumber(listofStudents , numberofStudents, i);
 }
 
-void insertStudentDetails(struct Student students[] , int numberofStudents)
+void getStudentSubjectMarks(int marks[])
 {
 	int i;
-	int j;
 	int subjectMarks;
-	
-	for (i = 0 ;i < numberofStudents ; i++)
-	{
-		scanf("%d %99s", &students[i].rollNumber, students[i].studentName);
-		
-		for (j = 0; j < SUBJECT_COUNT; j++)
+	for (i = 0; i < SUBJECT_COUNT; i++)
 		{
 			if (scanf("%d", &subjectMarks) != 1)
 			{
@@ -182,8 +188,18 @@ void insertStudentDetails(struct Student students[] , int numberofStudents)
 				return;
 			}
 
-			students[i].marks[j] = subjectMarks;
+			marks[i] = subjectMarks;
 		}
+}
+
+void insertStudentDetails(struct Student students[] , int numberofStudents)
+{
+	int i;
+	
+	for (i = 0 ;i < numberofStudents ; i++)
+	{
+		scanf("%d %99s", &students[i].rollNumber, students[i].studentName);
+		getStudentSubjectMarks(students[i].marks);
 	}
 }
 
